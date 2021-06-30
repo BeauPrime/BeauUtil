dc18b2a6b3ca71066a8a842ccdd5f93c5cf24bc6
BeauPrime <beauchesne@beauprime.com>
2021-06-29 23:28:25 -0500

    * Added SafeMod, DegreeAngleDifference, and RadianAngleDifference to MathUtil

--- Assets/BeauUtil/MathUtil.cs
+++ Assets/BeauUtil/MathUtil.cs
@@ -7,6 +7,10 @@
  * Purpose: Math utility functions.
 */
 
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
 namespace BeauUtil
 {
     /// <summary>
@@ -30,6 +34,40 @@ namespace BeauUtil
         static public float Remap(float inValue, float inMin1, float inMax1, float inMin2, float inMax2)
         {
             return (inValue - inMin1) / (inMax1 - inMin1) * (inMax2 - inMin2) + inMin2;
+        }
+
+        /// <summary>
+        /// Performs a safe modulo that consistently wraps correctly for negative values.
+        /// </summary>
+        [MethodImpl(256)]
+        static public float SafeMod(float inA, float inB)
+        {
+            return ((inA % inB) + inB) % inB;
+        }
+
+        /// <summary>
+        /// Performs a safe modulo that consistently wraps correctly for negative values.
+        /// </summary>
+        [MethodImpl(256)]
+        static public float SafeMod(int inA, int inB)
+        {
+            return ((inA % inB) + inB) % inB;
+        }
+
+        /// <summary>
+        /// Returns the difference between two angles, in degrees.
+        /// </summary>
+        static public float DegreeAngleDifference(float inDegA, float inDegB)
+        {
+            return SafeMod(180 + inDegB - inDegA, 360) - 180;
+        }
+
+        /// <summary>
+        /// Returns the difference between two angles, in radians.
+        /// </summary>
+        static public float RadianAngleDifference(float inRadA, float inRadB)
+        {
+            return SafeMod(Mathf.PI + inRadB - inRadA, Mathf.PI * 2) - Mathf.PI;
         }
     }
 }
